c530b473f241b3e654bac0e8dbf9ae16950d507f
Shohin Khamidjanov <skhamidjanov@verisign.com>
2018-02-19 14:46:09 -0500

    validations

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>
-        PostsNew Page
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this._renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this._renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this._renderField}
+        />
+      </form>
+    );
+  }
+
+  _renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input} // pre-generated event handlers
+        />
       </div>
     );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    errors.title = 'Enter a title that is at least 3 characters!';
+  }
+  if (!values.categories) {
+    errors.title = 'Enter categories!';
+  }
+  if (!values.content) {
+    errors.title = 'Enter some content!';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  validate
+})(PostsNew);
