2809d878314eecfb9a97cc88746d648d57d22281
Shohin Khamidjanov <skhamidjanov@verisign.com>
2018-02-19 21:34:46 -0500

    more checks

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
-    const { id } =  this.props.match.params;
-    this.props.fetchPost(id);
+    if (!this.props.post) {
+      const { id } =  this.props.match.params;
+      this.props.fetchPost(id);
+    }
   }
 
   render() {
@@ -14,9 +17,10 @@ class PostsShow extends Component {
     if (!post) {
       return <div>Loading...</div>;
     }
-    
+
     return (
       <div>
+        <Link to="/">Back To Main Page</Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
